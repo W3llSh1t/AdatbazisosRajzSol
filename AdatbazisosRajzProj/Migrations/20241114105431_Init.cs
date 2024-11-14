b7277f70d0413689214d96be1268ad8bb2c5a14d
W3llSh1t <lehel.daniel.zsolt@blathy.info>
2024-11-14 12:22:19 +0100

    kezdes

---
+++ AdatbazisosRajzProj/Migrations/20241114105431_Init.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdatbazisosRajzProj.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Draws",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Draws", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Points",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    X = table.Column<int>(type: "int", nullable: false),
+                    Y = table.Column<int>(type: "int", nullable: false),
+                    Color = table.Column<int>(type: "int", nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    RajzID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Points", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Points_Draws_RajzID",
+                        column: x => x.RajzID,
+                        principalTable: "Draws",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Points_RajzID",
+                table: "Points",
+                column: "RajzID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Points");
+
+            migrationBuilder.DropTable(
+                name: "Draws");
+        }
+    }
+}
--- AdatbazisosRajzProj/Program.cs
+++ AdatbazisosRajzProj/Program.cs
@@ -1,10 +1,65 @@
-﻿namespace AdatbazisosRajzProj
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdatbazisosRajzProj
 {
+    //rajz.Pontok.Add(new Pont { X = 1, Y = 1, Color = ConsoleColor.Red, Symbol = 'O', Rajz = rajz });
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Rajz> Draws { get; set; }
+        public DbSet<Pont> Points { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RajzAdatbazis;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+    }
+    public class Rajz
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required, StringLength(100)]
+        public string? Name { get; set; }
+        public List<Pont> Pontok { get; } = new List<Pont>();
+    }
+    public class Pont
+    {
+        [Key]
+        public int ID { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        [Required]
+        public ConsoleColor? Color { get; set; }
+        public char Symbol { get; set; }
+        [Required]
+        public Rajz Rajz { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            string[] Files = []; 
+            ApplicationDbContext context = new ApplicationDbContext();
+            context.Draws.Add(new Rajz { Name = "Kör" });
+            context.Points.Append(new Pont { X = 1, Y = 1, Color = ConsoleColor.Red, Symbol = 'O', Rajz = { } });
+            context.Points.Append(new Pont { X = 2, Y = 2, Color = ConsoleColor.Red, Symbol = 'O', Rajz = { } });
+            foreach (var rajz in context.Draws)
+            {
+                Files.Append(rajz.Name);
+                Console.WriteLine(rajz.Name);
+                foreach (var pont in rajz.Pontok)
+                {
+                    Console.WriteLine(pont.X);
+                    Console.WriteLine(pont.Y);
+                    Console.WriteLine(pont.Color);
+                    Console.WriteLine(pont.Symbol);
+                    Console.WriteLine(pont.Rajz);
+                }
+            }
             
+            context.SaveChanges();
         }
     }
 }
